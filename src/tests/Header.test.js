77c49d1fa0ce3f1ceed6c04b3400e957d5cd276f
Cole Hart <cole@colehart.com>
2018-12-27 15:56:32 -0700

    Add error message to Header with styling and passing tests

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Header } from '../containers/Header';
-
+import { Header, mapStateToProps } from '../containers/Header';
+import { mockDefaultState, mockFullState } from './testMocks';
 describe('Header', () => {
   let wrapper;
 
@@ -11,5 +11,24 @@ describe('Header', () => {
 
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('matches error snapshot', () => {
+    wrapper = shallow(<Header caughtError={'There is an error'}/>)
+
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  describe('matchStateToProps', () => {
+    it('should parse caughtError from state', () => {
+      const expected1 = ''
+
+      const mappedProps1 = mapStateToProps(mockDefaultState)
+      expect(mappedProps1.caughtError).toEqual(expected1)
+
+      const expected2 = 'Error: Bad API Key.'
+      const mappedProps2 = mapStateToProps(mockFullState)
+      expect(mappedProps2.caughtError).toEqual(expected2)
+    })
   })
 })
--- src/tests/testMocks.js
+++ src/tests/testMocks.js
@@ -1,7 +1,17 @@
-export const mockErrorMessage = 'Error: path not found'
+export const mockErrorMessage = 'Error: Bad API Key.'
 export const mockTag = 'Work'
 export const mockText = 'This is my note.'
 export const mockNote = {
   tag: mockTag,
   text: mockText
+}
+export const mockDefaultState = {
+  isLoading: false,
+  caughtError: '',
+  notes: []
+}
+export const mockFullState = {
+  isLoading: true,
+  caughtError: mockErrorMessage,
+  notes: [mockNote]
 }
