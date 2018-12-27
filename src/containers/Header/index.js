dfb3e31f9f982889c7ce00463e3029858c8221c8
Cole Hart <cole@colehart.com>
2018-12-27 13:51:45 -0700

    Change logo from svg to png and refactor styling

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import nordIcon from '../../'
+import nordIcon from '../../assets/images/nordstrom-notes-logo.png'
 import './Header.css';
 
 export const Header = () => {
   return (
     <header className='Header'>
-      Header
+      {/*
+      <div
+        className={ caughtError ? 'h-error' : 'hidden'}
+      >
+        { caughtError }
+      </div>
+      */}
+
+      <Link
+        to='/'
+        className='h-logo'
+        aria-label='Return to the Take Note Form'
+      >
+        <img
+          src={nordIcon}
+          alt='The Nordstrom Notes logo'
+        />
+      </Link>
     </header>
   )
 }
