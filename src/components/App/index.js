e72d1c5edde4ed14540d8664edfb7a71b01239df
Cole Hart <cole@colehart.com>
2018-12-27 12:02:29 -0700

    Add Routes and 404 page to App. Convert app to functional component and update refs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import Header from '../../containers/Header'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Nordstrom Notes
-        </header>
-      </div>
-    );
-  }
+export const App = () => {
+  const noMatch = ({ location }) => (
+    <div className='a-404'>
+      <h2>404 - Path not found.</h2>
+      <p>No routes match {location.pathname}.</p>
+      <p>Please click the the 'Take Note' or 'Notable Notes' buttons or your browser's forward and back buttons to navigate to your desired destination. The Nordstrom Notes logo will return you to the Take Note page.</p>
+    </div>
+  )
+  return (
+    <div className="App">
+      <header className="App-header">
+        Nordstrom Notes Header
+      </header>
+      <Switch>
+        <Route
+          exact path='/'
+          component={ NoteForm }
+        />
+        <Route
+          exact path='/notes'
+          component={ NotesList }
+        />
+        <Route
+          component={ noMatch }
+        />
+      </Switch>
+    </div>
+  );
 }
 
-export default App;
