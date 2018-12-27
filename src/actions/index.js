703db9ffbb4b879b4358ce9179a75f0d9a62970c
Cole Hart <cole@colehart.com>
2018-12-27 10:08:31 -0700

    Add note actions and refactor index.html

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool,
+})
+
+export const caughtError = (errorMessage) => ({
+  type: 'CAUGHT_ERROR',
+  errorMessage,
+})
+
+export const addNote = (tag, text) => {
+  type: 'ADD_NOTE',
+  tag,
+  text,
+}
