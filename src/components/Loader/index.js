aa646b62af251523048e7bf5e369a7ba3df234fe
Cole Hart <cole@colehart.com>
2018-12-27 16:15:59 -0700

    Add Loader component, styling and snapshot test

---
+++ src/components/Loader/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loader.css'
+
+export const Loader = () => {
+  return (
+    <div className='Loader'>
+      <iframe
+        className="l-giphy-embed"
+        src="https://giphy.com/embed/xkmQfH1TB0dLW"
+        frameBorder="0"
+        title="typewriter"
+        allowFullScreen
+      >
+      </iframe>
+    </div>
+  )
+}
