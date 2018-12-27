5e434710f4fdad939193290d775694ee67bcb59b
Cole Hart <cole@colehart.com>
2018-12-27 13:27:37 -0700

    Move images to src/assets, add Header styling

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import nordIcon from '../../'
 import './Header.css';
 
 export const Header = () => {
