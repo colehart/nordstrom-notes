f71d5814a77df63289ba1dfc3a4e58a32c1f0ae1
Cole Hart <cole@colehart.com>
2018-12-27 17:45:21 -0700

    Refactor 404 html tag and styling to streamline appearance of everything in the Main / main area

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// will have to change import style for containers once connected to store
 import Header from '../../containers/Header'
-import { NoteForm } from '../../containers/NoteForm'
-import { NotesList } from '../../containers/NotesList'
+import Main from '../../containers/Main'
 import './App.css';
 
 export const App = () => {
   const noMatch = ({ location }) => (
-    <div className='a-404'>
+    <main className='a-404'>
       <h2>404 &ndash; Path not found.</h2>
       <p>No routes match <code>{location.pathname}</code>.</p>
       <p>Please click the the 'Take Note' or 'Notable Notes' buttons above or your browser's forward and back buttons to navigate to your desired destination.</p>
       <p>The Nordstrom Notes logo will return you to the Take Note page.</p>
-    </div>
+    </main>
   )
 
   return (
