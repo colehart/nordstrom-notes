4660107ec8ead324d281c845ad47a913ef9b2af9
Cole Hart <cole@colehart.com>
2018-12-28 12:55:31 -0700

    Begin table for notes

--- src/containers/NotesList/index.js
+++ src/containers/NotesList/index.js
@@ -1,41 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { caughtError } from '../../actions';
 import NotesFilter from '../NotesFilter';
 import './NotesList.css';
 
-export class NotesList extends Component {
-  constructor () {
-    super()
-    this.state = {
-      notes: [],
-    }
-  }
-
-  noNotes = () => (
-    <section className='nl-list'>
+export const NotesList = ({notes, caughtError}) => {
+  const noNotes = (
+    <div className='nl-no-notes'>
       <h3>You have no notes.</h3>
       <p>Please click the the 'Make Note' or your browser's back button to navigate to the Make Note form.</p>
       <p>The Nordstrom Notes logo will also return you to the Make Note page.</p>
-    </section>
+    </div>
   )
 
-  render() {
-    const { notes } = this.state;
-
+  const notesRows = notes.map((note, index) => {
+    console.log(note)
     return (
-      <section className='NotesList'>
-        <h2>Notable Notes</h2>
-        <NotesFilter />
-        {!notes.length ? this.noNotes() : ''}
-      </section>
+      <div className={`nl-row-${index}`} key={index}>
+        <section className={`nl-row-note`}>
+          {note.text}
+        </section>
+        <section className={`nl-row-tag`}>
+          {note.tag}
+        </section>
+        <section className={`nl-row-note`}>
+          {note.date || null}
+        </section>
+      </div>
     )
-  }
+  })
+
+  return (
+    <section className='NotesList'>
+      <h2>Notable Notes</h2>
+      <NotesFilter />
+      <section className='nl-list'>
+        { !notes.length ? noNotes : '' }
+        <div className='nl-table'>
+          <article className='nl-table-header'>
+            <div className='nl-header-title'>
+              Note
+            </div>
+            <div className='nl-header-title'>
+              Tag
+            </div>
+            <div className='nl-header-title'>
+              Date Created
+            </div>
+          </article>
+          <article className='nl-table-rows'>
+            { notesRows }
+          </article>
+        </div>
+      </section>
+    </section>
+  )
 }
 
 export const mapStateToProps = (state) => ({
-  notes: state.notes
+  notes: state.notes,
 })
 
 export const mapDispatchToProps = (dispatch) => ({
